f03fcfd9b28d773381481f4a39c2484cc443eb09
laurakomp <82443899+laurakomp@users.noreply.github.com>
2021-10-20 19:21:37 +0300

    added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dictionary</h1>
-      </header>
-      Hello
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer className="text-center">
+          <small>Coded by Laura Komp</small>
+        </footer>
+      </div>
     </div>
   );
 }
